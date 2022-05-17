4d1db03309e36bb81c7ed95cd14698d834878acd
Mark L  Mariscotes <99033220+mlmariscotes@users.noreply.github.com>
2022-05-17 10:27:29 +0800

    Adding comments

--- Roman Numeral Converter.js
+++ Roman Numeral Converter.js
@@ -6,14 +6,24 @@ All roman numerals answers should be provided in upper-case.
 */
 
 function convertToRoman(num) {
-  var roman = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
-  var str = '';
+  const roman = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+  let str = '';
 
-  for (var i of Object.keys(roman)) {
-    var q = Math.floor(num / roman[i]);
+  // Giving i the Roman Numeral in the roman array 
+  for (let i of Object.keys(roman)) {
+
+    // Input Number divided by the Roman Numeral value in the roman array
+    // Rounding down to num value
+    let q = Math.floor(num / roman[i]);
+    
     num -= q * roman[i];
-    str += i.repeat(q);
+
+    // Make i copy relative to the value of q
+    str += i.repeat(q);   
   }
 
   return str;
 }
+
+let romanNumeral = convertToRoman(1994); // Input Number
+console.log(romanNumeral); // Output: MCMXCIV
