228dc6174c5cd723037fe46d7865bf48a028f374
Mark L  Mariscotes <99033220+mlmariscotes@users.noreply.github.com>
2022-05-17 10:30:59 +0800

    Updating Output

--- Palindrome Checker.js
+++ Palindrome Checker.js
@@ -4,7 +4,6 @@ Return true if the given string is a palindrome. Otherwise, return false.
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 */
 
-// My Answer1
 function palindrome(str) {
   str = str.toLowerCase();
   str = str.replace(/[^a-z0-9]/gi, ''); //Removing non-alphanumeric characters.
@@ -12,12 +11,15 @@ function palindrome(str) {
   const reverseArray = array.reverse(""); 
   const reverseStr = reverseArray.join("");
   if (str == reverseStr) {
-    return true; //or console.log(`The word "${str}" is a Palindrome`);
+    // return true;
+    console.log(`The word "${str}" is a Palindrome`);
   } else {
-    return false; //or console.log(`The word "${str}" is not a Palindrome`);
+    //return false;
+    console.log(`The word "${str}" is not a Palindrome`);
   }
 }
 
 palindrome("2_A3*3#A2"); //input
+
 
 
